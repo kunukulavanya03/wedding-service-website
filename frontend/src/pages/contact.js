206a7408658adb771263a3d43991231836dab3fe
kunukulavanya03 <kunukulavanya03@gmail.com>
2025-11-28 15:47:10 +0530

    🚀 Initial commit - Generated with FigmaToFullApp

---
+++ frontend/src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Form from '../components/Form';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <Form />
+    </div>
+  );
+}
